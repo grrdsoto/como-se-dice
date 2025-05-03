8b2cfe638a2c0ecfa5fe4e15127bb5293f2c29fa
Gerardo <grrdnazar@gmail.com>
2025-05-03 15:47:44 -0500

    clean up old folders

--- client/src/context/UserContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-/**
- * Instantiating the user context that will be used throughout the game.
- */
-const UserContext = createContext();
-export default UserContext;
